8d774eb2fbce8529ae18f97e4373927934ffb3d4
noeljusw14 <matthewhnoel@gmail.com>
2019-12-22 21:56:35 -0500

    final part is coming up

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,79 +5,109 @@ var connection = mysql.createConnection({
   host: "localhost",
 
   // Your port; if not 3306
-  port: 3306,
+  port: 8889,
 
   // Your username
   user: "root",
 
   // Your password
-  password: "",
-  database: "bamazon"
+  password: "root",
+
+  // Your password
+  database: "bamazon_db"
+
 });
-var query = connection.query("")
 // Running this application will first display all of the items available for sale.
-connection.connect(function(err) {
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log(
+    "You are connected as id " + connection.threadId + "\n" +
+    "Welcome to Energy Unlimited!\n"
+  );
+
+  afterConnection()
+});
+function afterConnection() {
+  //Query database to display items in store
+  var query = connection.query("SELECT * FROM products", function (err, res) {
     if (err) throw err;
-    console.log("Welcome to Energy Unlimited!\n" +
-    "You are connected as id " + connection.threadId);
-    afterConnection()
-  });
-    function afterConnection() {
-      connection.query("SELECT * FROM products", function(err, res) {
-        if (err) throw err;
-        console.log(
-         "Name:" + res.product_name,
-         "Department:" + res.department_name,
-      "Price:" + res.price,
-      "Availalbe" + res.stock_quantity
-        );
-        connection.end();
-      });
+
+    for (var i = 0; i < res.length; i++) {
+      console.log(
+        "\n",
+        "ID:" + res[i].item_id,
+        "\nName:" + res[i].product_name,
+        "\nPrice:" + "$" + res[i].price,
+        "\nQTY Available:" + res[i].stock_quantity)
     }
-  
-  // questions
-  // * The first should ask them the ID of the product they would like to buy.
-  // * The second message should ask how many units of the product they would like to buy.
-inquirer
-.prompt([
-  {
-    type: "input",
-    name: "ID",
-    message: "What is the product ID of the item you would like to purchase?"
-  },
-  {
-    type: "input",
-    name: "Qty",
-    message: "How many units would you like?"
-  }
-]).then(function(answers){
-// Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
-function checkAvailability(){}
-// * If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
 
-})
+  })
+  console.log(query.sql)
+  purchase()
+}
+function purchase() {
+
+  connection.query("SELECT * FROM products", function (err, res) {
+    if (err) throw err;
+    inquirer
+      .prompt([
+        {
+          name: "ID",
+          type: "rawlist",
+          choices: function () {
+            var choiceArray = [];
+
+            for (var i = 0; i < res.length; i++) {
+              choiceArray.push(
+                "ID:" + res[i].item_id + "|" +
+                "Name:" + res[i].product_name + "|" +
+                "Price:" + "$" + res[i].price)
+            }
+            return choiceArray;
+
+          },
+          message: "What is the ID of the product they would like to buy?"
+        },
+        {
+          name: "bid",
+          type: "input",
+          message: "How many would you like to purchase?"
+        }
+
+      ]).then(function (answer) {
+        var quantityOrder = parseInt(answer.bid);
+        var query = connection.query(
+          "UPDATE products SET ? WHERE ?",
+          [
+            {
+              stock_quantity: newStock
+            },
+            {
+              id: chosenItem.id
+            }
+          ],
+          function (err, res) {
+            if (error) throw err;
+            for (var i = 0; i < res.length; i++) {
+              var newStock = chosenItem.stock_quantity - quantityOrder;
+              if (res[i].stock_quantity > parseInt(answer.bid)) {
 
 
+                console.log("Order placed successfully!");
+                afterConnection();
+              }
+            
+          );
 
+      }
+        else {
+          // bid wasn't high enough, so apologize and start over
+          console.log("We dont have enought in stock!");
+          afterConnection();
 
-
-// ~~~~~~~Create a Product function~~~~~~~~~~
-  // function createProduct(){
-  //   console.log("Inserting a new product...\n");
-  // var query = connection.query(
-  //   "INSERT INTO products SET ?",
-  //   {
-  //     product_name:,
-  //     department_name:,
-  //     price:,
-  //     stock_quantity:
-  //   },
-    
-  // }
-  // 8. However, if your store _does_ have enough of the product, you should fulfill the customer's order.
-  //  * This means updating the SQL database to reflect the remaining quantity.
-  //  * Once the update goes through, show the customer the total cost of their purchase.
-  function updateProduct(){}
-  function deleteProduct(){}
-  
-  
+        }
+          
+        }
+      
+     
+  })})}
---
+++ seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) VALUES
+(7,"High Capacity Solar Power Bank  10000Mah", "Mobile", 20, 100),
+(8,"External Battery & Solar Panel Power Bank 72000Mah for Laptop", "Large_Capacity", 120, 20),
+(9,"AC/DC Switching Solar Power Bank 500WH for outdoors", "Large_Capacity", 120, 25),
+(10,"New Magnetic Mini Power Bank Capsule 1000Mah - 4 Pack", "Mobile", 55, 150),
+(11,"Mini Power Bank Charger - Promotional Gift", "Mobile", 8, 50)
