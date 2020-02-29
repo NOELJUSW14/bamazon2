676f7b16b01a486c5518b6c64c93f3914c6e37df
noeljusw14 <matthewhnoel@gmail.com>
2020-02-29 10:13:14 -0500

    Added video

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,122 +1,132 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
+var mysql = require('mysql')
+var inquirer = require('inquirer')
+var Table = require('cli-table3');
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: '127.0.0.1',
 
   // Your port; if not 3306
   port: 8889,
 
   // Your username
-  user: "root",
+  user: 'root',
 
   // Your password
-  password: "root",
+  password: 'root',
 
   // Your DB
-  database: "bamazon_db"
-
-});
+  database: 'bamazon_db',
+})
 // Running this application will first display all of the items available for sale.
-connection.connect(function (err) {
+connection.connect(function(err){
   if (err) {
-    console.log("Cannot connect!")
+    console.log('Cannot connect!')
   } else {
     console.log(
-      "You are connected as id " + connection.threadId + "\n" +
-      "Welcome to Energy Unlimited!\n"
+      'You are connected as id ' +
+        connection.threadId +
+        '\n' +
+        'Welcome to Energy Unlimited!\n',
     );
-    afterConnection()
-    
+    display()
+   
   }
-  
-  
-  
 });
-function afterConnection() {
-  //Query database to display items in store
-  var query = connection.query("SELECT * FROM products", function (err, res) {
-    if (err) {
-      console.log(err);
-    } else {
-      for (var i = 0; i < res.length; i++) {
-        console.log(
-          "\n",
-          "ID:" + res[i].item_id,
-          "\nName:" + res[i].product_name,
-          "\nPrice:" + "$" + res[i].price,
-          "\nQTY Available:" + res[i].stock_quantity)
+  
+var display = function() {
+connection.query('SELECT * FROM products', function(err, res) {
+    if (err) throw err;
+var table = new Table({
+  head: ['ID', 'Name','Price','Qty Available'],
+  colWidths: [10, 60, 10, 10],
+  colAligns:["center","left", "right"],
+  style:{ 'padding-left': 0, 'padding-right': 0 }
+});
+
+for (var i = 0; i < res.length; i++) {
+  table.push([res[i].item_id,res[i].product_name,"$"+ res[i].price,res[i].stock_quantity])
+}
+  console.log(table.toString());
+  afterDisplay() 
+});
+}
+
+function afterDisplay() {
+  inquirer.prompt([
+		{
+			name: "action",
+			type: "list",
+			choices: ["View items for sale", "Leave the store"],
+			message: "Please select what you would like to do."
+		}]).then(function(action) {
+      // if user wants to view items, run the view items function
+      if (action.action === "View items for sale") {
+        purchase();
+        // if user wants to leave, run exit function
+      } else if (action.action === "Leave the store") {
+        exit();
       }
-    }
-
-
-  })
-  console.log(query.sql)
-  purchase()
-}
+    });
+  }
+  //Query database to display items in store
+  // var query = connection.query('SELECT * FROM products', function(err, res) {
+  //   if (err){
+  //     throw err;
+  //   } else {
+  //     for (var i = 0; i < res.length; i++) {
+  //       console.log(
+  //         '\n',
+  //         'ID:' + res[i].item_id,
+  //         '\nName:' + res[i].product_name,
+  //       )
+  //     }
+  //   }
+  // })
+  // console.log(query.sql)
+  // purchase()
 function purchase() {
-
-  connection.query("SELECT * FROM products", function (err, res) {
-    if (err){
-      console.log(err) 
-      return
-    };
-    inquirer
-      .prompt([
+  inquirer
+      .prompt(
         {
-          name: "ID",
-          type: "rawlist",
-          choices: function () {
-            var choiceArray = [];
-
-            for (var i = 0; i < res.length; i++) {
-              choiceArray.push(
-                "ID:" + res[i].item_id + "|" +
-                "Name:" + res[i].product_name + "|" +
-                "Price:" + "$" + res[i].price)
-            }
-            return choiceArray;
-
-          },
-          message: "What is the ID of the product they would like to buy?"
-        },
-        {
-          name: "bid",
+          name: 'ID',
           type: "input",
-          message: "How many would you like to purchase?"
-        }
-
-      ]).then(function (answer) {
-        var quantityOrder = parseInt(answer.bid);
-        var query = connection.query(
-          "UPDATE products SET ? WHERE ?",
-          [
-            {
-              stock_quantity: newStock
-            },
-            {
-              id: chosenItem.id
+          message: 'What is the ID of the product they would like to buy?',
+        })
+      .then(function(answer) {
+        var chosenItem = answer.ID;
+        connection.query('SELECT * FROM products', function(err, res) {
+          if (err) {
+            console.log("Not Available!");
+          } if(chosenItem === 0){
+            console.log("Make another selection!")
+            purchase()
+          }else{
+          inquirer
+            .prompt({
+              name: 'bid',
+              type: 'input',
+              message: 'How many would you like to purchase?',
+            })
+            .then(function(answer2){
+              var quantityOrder = parseInt(answer2.stock_quantity);
+              if(quantityOrder > res.stock_quantity){
+                console.log("Please adjust your qty");
+                purchase();
+              }else{
+                var newStock = chosenItem.stock_quantity - quantityOrder; //or use answer1 as reference
+                connection.query(
+                'UPDATE products SET stock_quantity =' +
+              newStock +
+              'WHERE id =' +
+              chosenItem,
+            function(err, resUpdate) {
+              if (err) console.log(err);
+              console.log('Order placed successfully!')
+              connection.end()
             }
-          ],
-          function (err, res) {
-            if (error) throw err;
-            for (var i = 0; i < res.length; i++) {
-              var newStock = chosenItem.stock_quantity - quantityOrder;
-              if (res[i].stock_quantity > parseInt(answer.bid)) {
-
-
-                console.log("Order placed successfully!");
-                afterConnection();
-              } else {
-                // bid wasn't high enough, so apologize and start over
-                console.log("We dont have enought in stock!");
-                afterConnection();
-
-              }
-
-            }
-          })
-      })
-  })
-}
-
+        )}})}})})}
+            // else {
+            //   // bid wasn't high enough, so apologize and start over
+            //   console.log('We dont have enought in stock!')
+            //   afterConnection()
+            //  
